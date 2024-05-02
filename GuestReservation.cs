f953d21bed95b1b643dbd50ae0a2228f9d929976
chad labore <chad.labore@marquette.edu>
2024-05-02 15:09:46 -0500

    GuestReservation

---
+++ GuestReservation.cs
@@ -0,0 +1,14 @@
+﻿namespace CourseProject001;
+
+public class GuestReservation
+{
+    public Guest c {get; set;}
+    public Reservtion a {get; set;}
+
+    public GuestReservation(Guest c, Reservation a)
+    {
+        this.c = c;
+        this.a = a;
+    }
+
+}
